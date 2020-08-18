bd2031e7d32640be26b77731866ccaa839c6f84a
Taylor Somers <tsomers@yandex.com>
2020-08-18 19:01:14 -0400

    Update files to use Newtonsoft and Restsharp
    Co-authored-by: SPdowns <spdowns@protonmail.com>
    Co-authored-by: Tristan Emmerson <tristan@stickerslug.com>
    Co-authored-by: RichterBros <erich.richter@gmail.com>

--- BeastClient/Program.cs
+++ BeastClient/Program.cs
@@ -1,24 +1,37 @@
-﻿using System;
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using RestSharp;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
 
-namespace BeastClient
+namespace ApiTest
 {
-    public class Program
+    class Program
     {
-        public static void Main(string[] args)
+        static void Main()
         {
-            CreateWebHostBuilder(args).Build().Run();
+            var apiCallTask = ApiHelper.ApiCall("[YOUR-API-KEY-HERE]");
+            var result = apiCallTask.Result;
+            JObject jsonResponse = JsonConvert.DeserializeObject<JObject>(result);
+            Console.WriteLine(jsonResponse["results"]);
         }
+    }
 
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+    class ApiHelper
+    {
+        public static async Task<string> ApiCall(string apiKey)
+        {
+            RestClient client = new RestClient("https://api.nytimes.com/svc/topstories/v2");
+            RestRequest request = new RestRequest($"home.json?api-key={apiKey}", Method.GET);
+            var response = await client.ExecuteTaskAsync(request);
+            return response.Content;
+        }
     }
 }
